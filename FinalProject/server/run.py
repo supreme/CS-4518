718f58723cbcace1df9fe95e73d90da722dbc5f0
Stephen Andrews <sandrews.r@gmail.com>
2018-02-19 17:01:35 -0500

    Docker config for container deployment.

--- FinalProject/server/run.py
+++ FinalProject/server/run.py
@@ -4,9 +4,13 @@ server.py
 Created by Stephen Andrews, February 17th, 2018.
 """
 
+import os
+
 from book_smart import create_app
 
 app = create_app()
+port = int(os.environ.get('PORT', default=8080))
+debug = os.environ.get('DEBUG', default=False)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=debug)
